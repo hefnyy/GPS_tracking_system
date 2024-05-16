d0dc9292853506906625fe9413711471099eed83
hefnyy <mohammedahefny1111@gmail.com>
2024-05-16 22:49:19 +0300

    Main Updated

--- SRC/App/main.c
+++ SRC/App/main.c
@@ -10,56 +10,104 @@
 #include "GPS.h"
 #include "UART.h"
 #include "SysTick.h"
+#include "LCD.h"
 
-#define CB 0x2A									//Hexadecimal for (*)
+#define CR 0x0D					// 0x0D == Enter
+#define CB 0x2A					// 0x2A == '*'
+
+
 
 float total_Distance=0.0;
+char dis[10]={0};
+
 
 /// ************************************************************************************************************************************* /// 
  
 	int main() {
 		
-		float receivedLat = 0 , receivedLong = 0 , Speed = 0 , oldLat = 0 , oldLong = 0 , Temp = 0 ;		//initialization of variables
-		
-		float Lon[50]={0};						//Array of float to save Longitude points in EEPROM to plot them on map;
-		float Lat[50]={0};						//Array of float to save Latitude points in EEPROM to plot them on map;
-		int k=0;							//Counter for Lat[]
-		int l=0;							//Counter for Lon[]
 
-		UART0_init();					//initialize UART0 for PC communication
-		GPIO_Init_PortF();				//initialize Port F for LEDs & Switches
-		UART1_Init();					//initialize UART0 for GPS communication
+		float receivedLat = 0 , receivedLong = 0 , Speed = 0 , oldLat = 0 , oldLong = 0 , Temp = 0 ;
 		
+		char tempp[10];
+		char Lat[301]={0};				//trajectory	// 301 = 150(lat)+150(long)+1(newline char =="\n")
+		char Lon[150]={0};				//trajectory
+		char lat[15]={0};					// array to convert the float value of lat to char of array
+		char lon[15]={0};					// array to convert the float value of lon to char of array
 		
+///***** Useful Counters *****///		
+		int b;		
+		int t;
+		int k=0;									
+		int l=0;									
+		char c;
+		
+///***** Initializations *****///		
+		GPIO_Init_PortF();
+		UART0_Init();
+		UART1_Init();
+		SysTick_Init();
+		LCD_Init();
+		
+		GPIO_PORTF_DATA_R |= 0x04;		
+		LCD_Cmd(0x80);
+		LCD_String_modified("Welcome ");
+		
+		SysTick_Wait10ms(300);
 
-		
 	while(1){
+				
+		SysTick_Wait10ms(200);
+		GPIO_PORTF_DATA_R &= ~0x04;
 		
 		while(total_Distance<100){
-			
-			GPIO_SetLedValue(GPIO_GREEN_LED , GPIO_LED_OFF);				//make sure that Green & Blue LEDs are turned Off
-			GPIO_SetLedValue(GPIO_BLUE_LED , GPIO_LED_OFF);
-			GPIO_SetLedValue(GPIO_RED_LED , GPIO_LED_ON);					//TurnOn RedLED
-			
-			GPS_read(&receivedLat , &receivedLong , &Speed);
-			
-		if(oldLat != 0 && oldLong != 0){
-			Temp = Get_Distance(receivedLat , receivedLong , oldLat , oldLong);		//Calculate distance between current point and previous point 
-			total_Distance = total_Distance + Temp ;								//Cumulate total distance
+				
+				GPIO_PORTF_DATA_R |= 0x08;
+				
+				GPS_read(&receivedLat , &receivedLong , &Speed);
+				
+				if(Speed >= 1){				
+					GPIO_PORTF_DATA_R |= 0x04;
+					ToDegree_modified(&receivedLat);
+					ToDegree_modified(&receivedLong);
+					sprintf(lat,"%f",receivedLat);
+					sprintf(lon,"%f",receivedLong);
+					for(b = 0; b < 1; b++){
+						append_with_comma(Lat, 301, lat);
+						append_with_comma(Lon, 150, lon);
+					}
+			if(oldLat != 0 && oldLong != 0){
+					Temp = Get_Distance_modified(receivedLat , receivedLong , oldLat , oldLong);
+					total_Distance = total_Distance + Temp ;
+				}
+					sprintf(dis ,"%.3f",total_Distance);
+					LCD_Cmd(0x01);
+					LCD_Cmd(0x80);
+					LCD_String_modified("Total Distance = ");
+					LCD_Cmd(0xC0);
+					LCD_String_modified(dis);
+					oldLong = receivedLong;
+					oldLat = receivedLat;	
+			}
+				GPIO_PORTF_DATA_R &= ~0x04;
 		}
 		
-		oldLong = receivedLong;						//Update oldlong point
-		oldLat = receivedLat;						//Update oldlat point
-		Lon[l++] = oldLong;							//Assign values for trajectory (Longtitude Cooredinates)
-		Lat[k++] = oldLat;							//Assign Values for trajectory (Latitude Cooredinates)
-		}
+		///***** Exceeded 100 meters *****///
+		GPIO_PORTF_DATA_R &= ~0x08;
+		GPIO_PORTF_DATA_R |= 0x02;
+		///***** Concatenate the lat and long array to send them to trajectory mapping *****///
+		strcat(Lat,",");
+		strcat(Lat,Lon);
+		strcat(Lat,"\n");
 		
-		GPIO_SetLedValue(GPIO_RED_LED , GPIO_LED_OFF);								//when reach the destination (total Distance > 100) turnOff RedLED and turnOn Blue & Green LEDs
-		GPIO_SetLedValue(GPIO_BLUE_LED , GPIO_LED_ON);
-		GPIO_SetLedValue(GPIO_GREEN_LED , GPIO_LED_ON);
-		}
-	
-
+		LCD_Cmd(0x01);
+		LCD_Cmd(0x80);
+		LCD_String_modified("Final Dest.");
+		Send_Draw_modified(Lat);
+		
+//		for(t=0 ; t<4 ; t++){
+//			Send_Draw();
+//		}
+	}		
     return 0;
 }
 
